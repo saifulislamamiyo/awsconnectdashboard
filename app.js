e14f8e5fa2140fd15358696183638884de8b4d0f
asiftyro <asif.tyro@gmail.com>
2022-10-19 20:59:34 +0600

    Flash message added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const flash = require('express-flash');
 
 /* Import routes */
 const campaignsRouter = require('./routes/campaigns');
@@ -20,7 +22,14 @@ app.set('view engine', 'html');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('Todo: Load from env config'));
+app.use(session({
+  secret: 'Todo: Load from env config',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true },
+}));
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* Register routes */
@@ -31,12 +40,12 @@ app.use('/dashboard', dashboardRouter);
 app.use('/custom-report', customReportRouter);
 
 /* Catch 404 and forward to error handler */
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 /* Error handler */
-app.use((err, req, res, next) =>{
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
