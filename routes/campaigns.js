abdb1275f63ab4077ad1906b17c1e05a42930393
asiftyro <asif.tyro@gmail.com>
2022-10-20 19:23:41 +0600

    Form data cleaning and validation.

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -3,11 +3,13 @@ const express = require('express');
 const router = express.Router();
 const connectClient = require('../libs/connectclient');
 
-router.get('/', async (req, res, next) => {
+const listCampaigns = async (req, res, next) => {
   const command = new ListQueuesCommand({ InstanceId: process.env.CONNECT_INSTANCE_ID });
   const campaigns = await connectClient.send(command);
-  console.log(campaigns.QueueSummaryList);
   res.render('campaigns', { title: 'Campaigns', campaigns: campaigns.QueueSummaryList });
-});
+}
+
+router.get('/', listCampaigns);
+router.get('/campaigns', listCampaigns);
 
 module.exports = router;
--- routes/createcampaign.js
+++ routes/createcampaign.js
@@ -5,85 +5,117 @@ const router = express.Router();
 const connectClient = require('../libs/connectclient')
 
 const campaignForm = async (formData) => {
-    const InstanceId = { InstanceId: process.env.CONNECT_INSTANCE_ID }
+  const InstanceId = { InstanceId: process.env.CONNECT_INSTANCE_ID }
 
-    let fromListPhoneNumbersCommand = (await connectClient.send(new ListPhoneNumbersCommand({...InstanceId})));
-    let phoneNumbers = [];
-    fromListPhoneNumbersCommand.PhoneNumberSummaryList.forEach((item)=>{
-        phoneNumbers.push({Id:item.Id, Name:item.PhoneNumber});
-    });
+  let fromListPhoneNumbersCommand = (await connectClient.send(new ListPhoneNumbersCommand({ ...InstanceId })));
+  let phoneNumbers = [];
+  fromListPhoneNumbersCommand.PhoneNumberSummaryList.forEach((item) => {
+    phoneNumbers.push({ Id: item.Id, Name: item.PhoneNumber });
+  });
 
-    let hoursOfOperations = (await connectClient.send(new ListHoursOfOperationsCommand({...InstanceId}))).HoursOfOperationSummaryList;
-    
-    let form =  {
-        formName: "campaignForm",
-        formMethod: "post",
-        formAction: "/create-campaign",
-        formFields: {
-            campaignName: {
-                type: "text",
-                label: "Campaign Name",
-                attr: "",
-                data: formData.campaignName || "",
-                error: '',
-            },
-            campaignDescription: {
-                type: "text",
-                label: "Campaign Description",
-                attr: "",
-                data: formData.campaignDescription||"",
-                error: '',
-            },
-            phoneNumber: {
-                type: "select",
-                label: "Phone Number",
-                attr: "",
-                data: phoneNumbers,
-                selected: "",
-                error: '',
-            },
-            hoursOfOperation: {
-                type: "select",
-                label: "Hours of Operation",
-                attr: "",
-                data: hoursOfOperations,
-                selected: formData.hoursOfOperation||"",
-                error: '',
-            },
-        }
-    };
-    return form;
+  let hoursOfOperations = (await connectClient.send(new ListHoursOfOperationsCommand({ ...InstanceId }))).HoursOfOperationSummaryList;
+
+  let form = {
+    formName: "campaignForm",
+    formMethod: "post",
+    formAction: "/create-campaign",
+    formFields: {
+      campaignName: {
+        type: "text",
+        label: "Campaign Name",
+        attr: "",
+        data: formData.campaignName || "",
+        error: '',
+      },
+      campaignDescription: {
+        type: "text",
+        label: "Campaign Description",
+        attr: "",
+        data: formData.campaignDescription || "",
+        error: '',
+      },
+      phoneNumber: {
+        type: "select",
+        label: "Phone Number",
+        attr: "",
+        data: phoneNumbers,
+        selected: formData.phoneNumber || "",
+        error: '',
+      },
+      hoursOfOperation: {
+        type: "select",
+        label: "Hours of Operation",
+        attr: "",
+        data: hoursOfOperations,
+        selected: formData.hoursOfOperation || "",
+        error: '',
+      },
+    }
+  };
+
+  return form;
 }
 
+const validatedCampaignForm = async (formData) => {
+  let cleanData = {
+    campaignName: String(formData.campaignName).trim() || "",
+    campaignDescription: String(formData.campaignDescription).trim() || "",
+    phoneNumber: String(formData.phoneNumber).trim() || "",
+    hoursOfOperation: String(formData.hoursOfOperation).trim() || "",
+  }
+
+  let form = await campaignForm(formData);
+
+  let validationPassed = true;
+  let campaignNameList = []
+  if (cleanData.campaignName == "" || campaignNameList.includes(cleanData.campaignName)) {
+    form.formFields.campaignName.error = "Unique Campaign Name is required.";
+    validationPassed = false
+  }
+  if (cleanData.phoneNumber == "") {
+    form.formFields.phoneNumber.error = "Phone Number is required.";
+    validationPassed = false
+  }
+  if (cleanData.hoursOfOperation == "") {
+    form.formFields.hoursOfOperation.error = "Hours of Operation is required.";
+    validationPassed = false
+  }
+  return { form, cleanData, validationPassed };
+}
 
 router.get('/', async (req, res, next) => {
-    res.render('createcampaign', {
-        title: 'Create Campaign',
-        form: await campaignForm(req.body),
-    });
+  return res.render('createcampaign', {
+    title: 'Create Campaign',
+    form: await campaignForm(req.body),
+  });
 });
 
 router.post('/', async (req, res, next) => {
-    console.log(
-        req.body.campaignName,
-        req.body.campaignDescription,
-        req.body.phoneNumber,
-        req.body.hoursOfOperation
-    );
 
-    param = {
+  let form = await validatedCampaignForm(req.body);
+
+  if (!form.validationPassed) {
+    return res.render('createcampaign', {
+      title: 'Create Campaign',
+      form: form.form,
+    });
+  } else {
+    let param = {
         InstanceId: process.env.CONNECT_INSTANCE_ID,
-        Name: req.body.campaignName,
-        Description: req.body.campaignDescription,
-        HoursOfOperationId: req.body.hoursOfOperation,
+        Name: form.cleanData.campaignName,
+        Description: form.cleanData.campaignDescription,
+        HoursOfOperationId: form.cleanData.hoursOfOperation,
         MaxContacts: 1,
         OutboundCallerConfig: {
-            OutboundCallerIdNumberId: req.body.phoneNumber,
+            OutboundCallerIdNumberId: form.cleanData.phoneNumber,
         }
     }
-    let command = new CreateQueueCommand(param);
-    let respo = await connectClient.send(command);
-    res.redirect("/")
+    // let command = new CreateQueueCommand(param);
+    // let respo = await connectClient.send(command);
+    req.flash('success', 'Campaign created successfully')
+    res.redirect("/campaigns");
+  }
+
 });
 
 module.exports = router;
