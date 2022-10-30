cec2e4dd73f351039d22795c600f0da9e1d6f5ae
Asif R. Porosh <asif.tyro@gmail.com>
2022-10-30 12:26:15 +0600

    Removed non-standard disabled queues from /agent-distribution

--- libs/utils.js
+++ libs/utils.js
@@ -1,5 +1,41 @@
+const { awsInstance } = require("./awsconfigloader");
+const connectClient = require("./connectclient");
+const {
+  SearchQueuesCommand,
+} = require("@aws-sdk/client-connect");
+
+
 const asyncConLog = async (val) => {
   console.log(val)
 }
 
-module.exports = {asyncConLog}
+const getStandardQueues = async () => {
+  let nextToken = "";
+  let queues = [];
+  do {
+    let param = {
+      InstanceId: awsInstance,
+      SearchCriteria: {
+        QueueTypeCondition: 'STANDARD',
+      }
+    };
+    if (nextToken != "") param.NextToken = nextToken;
+    let result = await connectClient.send(new SearchQueuesCommand(param));
+    nextToken = result.NextToken ?? '';
+    queues = queues.concat(result.Queues)
+  } while (nextToken != "")
+  return queues;
+}
+
+
+
+
+// async function test_getStandardQueues() {
+//   x = await getStandardQueues()
+//   console.log(x)
+// }
+
+// test_getStandardQueues()
+
+
+module.exports = { asyncConLog, getStandardQueues }
