9be79262640a67802696efae94efb9680fa5cf05
Saiful Islam <amio_098@live.com>
2023-01-20 15:16:22 +1100

    fixing spelling mistake

--- routes/inboundnumberprovision.js
+++ routes/inboundnumberprovision.js
@@ -39,7 +39,7 @@ router.get("/inbound-number-provision-save", async (req, res, next) => {
   let campaignName = req.query.campaignname;
   let phoneNumberId = req.query.phoneid;
   let phoneNumber = req.query.phoneNumber;
-  let phoneNumberDesc = req.query.phoneNumerDesc;
+  let phoneNumberDesc = req.query.phoneNumberDesc;
   let tollFreeNumber = req.query.tollFreeNumber;
 
   await insertPhoneNumberCampaignMap(
