53fc63a5d2c0572ca25f67a7d76064a2606e6a37
asiftyro <asif.tyro@gmail.com>
2023-02-02 14:57:07 +0600

    agent-dashboard bound to the logged-in user

--- utilities/password_hash.js
+++ utilities/password_hash.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 
-let usernames = ['nazim', 'saiful', 'asif'];
+let usernames = ['nazim', 'saiful', 'asif', 'steve.moran'];
 
 usernames.forEach((uname)=>{
   let hash = bcrypt.hashSync(uname, 15);
